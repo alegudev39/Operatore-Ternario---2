9e81ffdbc60b46e35dd34a8e54dc513cdf166ee0
AleDev <aledev@example.com>
2024-03-19 11:44:52 +0100

    cons

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//Copia il seguente programma Java e sostutuisci il costrutto if-else con l'operatore ternario
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+
+        String result = (value.length() >= 10) ? (result = "Lunghezza maggiore o uguale di 10") : (result = "Lunghezza minore di 10");
+
+//        String result;
+//        if (value.length() >= 10) {
+//            result = "Lunghezza maggiore o uguale di 10";
+//        } else {
+//            result = "Lunghezza minore di 10";
+//        }
+        return result;
+    }
+}
